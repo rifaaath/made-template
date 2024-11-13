7abf9bc0dc02ab91ae491479cff360e08c62dc3f
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2024-11-13 01:41:56 +0100

    Updated sh file

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+# Run chmod +x project/pipeline.sh initially
 
 # Install dependencies
 pip3 install pandas numpy requests
 
 # Run the main pipeline script
-python3 /project/pipeline.py
+python3 project/pipeline.py
