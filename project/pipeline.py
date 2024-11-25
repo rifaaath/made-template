b95e0290a57325a36b3149d51394b85446a58bf1
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2024-11-25 01:54:53 +0100

    Updated pipeline and added data-report

--- project/pipeline.py
+++ project/pipeline.py
@@ -3,63 +3,94 @@ import requests
 import zipfile
 import pandas as pd
 
-#education & economy
-url = [
-    "https://api.worldbank.org/v2/en/indicator/SE.PRM.ENRR?downloadformat=csv",
-    "https://api.worldbank.org/v2/en/indicator/NY.GDP.PCAP.CD?downloadformat=csv"
-    ]
+data_dir = "./data"
+os.makedirs(data_dir, exist_ok=True)
 
-data = [
-    os.path.join("./data/data1.zip"),
-    os.path.join("./data/data2.zip")
-]
-
-csv_dir = [
-    os.path.join("./data/data1.csv"),
-    os.path.join("./data/data2.csv")
-]
+def get_path(filename, ext):
+    return os.path.join(data_dir, f"{filename}.{ext}")
 
 #Download and save data
-try:
-    for i in range(2):
-        response = requests.get(url[i])
+def download_data(url, filename):
+    try:
+        response = requests.get(url)
         response.raise_for_status()
 
-        with open(data[i], "wb") as f:
+        data = get_path(filename, "zip")
+        with open(data, "wb") as f:
             f.write(response.content)
-        print("Zip File {} saved".format(i+1))
+        print("Zip File saved")
 
-        # Extract csv from zip
-        with zipfile.ZipFile(data[i], "r") as file:
+    except Exception as e:
+        print(e)
+
+def extract_data(filename):
+    data = get_path(filename, "zip")
+    try:
+        csv_dir = get_path(filename, "csv")
+        with zipfile.ZipFile(data, "r") as file:
             csv = file.namelist()[1]
             file.extract(csv, "./data")
-            os.rename(os.path.join("./data", csv), csv_dir[i])
-            # pd.read_csv(csv_dir[i], skiprows=2).to_csv(csv_dir[i])
+            os.rename(os.path.join("./data", csv), csv_dir)
         
-        os.remove(data[i])
-
-except Exception as e:
-    print(e)
+        os.remove(data)
+    except Exception as e:
+        print(e)
     
 #Clean Data
-try:
+def clean_data(filename):
     americas = [
-        "United States", "Canada", "Mexico", "Guatemala", "Honduras", "Costa Rica", "Brazil", "Argentina", "Colombia", "Chile"
-    ]
+            "United States", "Canada", "Mexico", "Guatemala", "Honduras", "Costa Rica", "Brazil", "Argentina", "Colombia", "Chile"
+        ]
     cols = ["Country Name", "Country Code"] + [str(i) for i in range(1970, 2024)]
+    try:
+        csv_dir = get_path(filename, "csv")
 
-    for i in range(2):
-        df = pd.read_csv(csv_dir[i], skiprows=4, header=0)
-        # print(df.head())
+        df = pd.read_csv(csv_dir, skiprows=4, header=0)
         df = df[cols]
         df = df[df["Country Name"].isin(americas)]
-        df = df.infer_objects()
-        # df.update(df.select_dtypes(include=['float64', 'int64']).interpolate(method='linear'))
-        df.loc[:, [str(i) for i in range(1970, 2024)]] = df.loc[:, [str(i) for i in range(1970, 2024)]].fillna(df.loc[:, [str(i) for i in range(1970, 2024)]].mean())
-        print(df)
-        df.to_csv(csv_dir[i])
-        print("Data {} extracted".format(i+1))
 
-except Exception as e:
-    print("Exception raised\n\n",e)
+        df = df.infer_objects()
+        df.loc[:, [str(i) for i in range(1970, 2024)]] = df.loc[:, [str(i) for i in range(1970, 2024)]].fillna(df.loc[:, [str(i) for i in range(1970, 2024)]].mean())
         
+        df.to_csv(csv_dir)
+        print("Data extracted")
+
+    except Exception as e:
+        print("Exception raised\n\n",e)
+
+def combine_file(filenames):
+    df = []
+    try:
+        for filename in filenames:
+            file = get_path(filename, "csv")
+            df.append(pd.read_csv(file))
+        
+        combined_df = df[0]
+        for df in df[1:]:
+            combined_df = pd.merge(combined_df, df, on="Country Name", how="inner", suffixes=('', '_other'))
+
+        combined_file_path = get_path("combined_data", "csv")
+        combined_df.to_csv(combined_file_path, index=False)
+        print(f"Combined file saved at {combined_file_path}")
+        
+    
+    except Exception as e:
+        print("Exception raised\n\n",e)
+
+
+def pipeline(url, filename):
+    download_data(url, filename)
+    extract_data(filename)
+    clean_data(filename)
+
+
+#education & economy
+data_url = [
+    "https://api.worldbank.org/v2/en/indicator/SE.PRM.ENRR?downloadformat=csv",
+    "https://api.worldbank.org/v2/en/indicator/NY.GDP.PCAP.CD?downloadformat=csv",
+]
+
+pipeline(data_url[0], "data1")
+pipeline(data_url[1], "data2")
+combine_file(["data1", "data2"])
+
