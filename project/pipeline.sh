7f4c5ba25ecce9f4dca646c8bfbf04fe6ed66a91
RIFAATH AMEEN <82785512+rifaaath@users.noreply.github.com>
2024-12-04 12:42:41 +0100

    Commented install req

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -2,7 +2,7 @@
 # Run chmod +x project/pipeline.sh
 
 # Install dependencies
-pip3 install -r requirements.txt
+# pip3 install -r project/requirements.txt
 
 # Run the main pipeline script
 python3 project/pipeline.py
--- project/tests.sh
+++ project/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Run unit and system tests
-pip3 install -r requirements.txt
+# pip3 install -r project/requirements.txt
 
 echo "Running unit tests.."
 python3 project/tests.py 
